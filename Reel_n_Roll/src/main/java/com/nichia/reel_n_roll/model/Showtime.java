5a4031cf7c321e4af17b1c11585e84366ddd3a62
ReemaMaryam <rmn1307@rit.edu>
2026-03-12 00:49:11 +0400

    Added Showtime model and showtime service methods

--- Reel_n_Roll/src/main/java/com/nichia/reel_n_roll/model/Showtime.java
+++ Reel_n_Roll/src/main/java/com/nichia/reel_n_roll/model/Showtime.java
@@ -1,45 +1,40 @@
 package com.nichia.reel_n_roll.model;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Showtime {
     private int showId;
-    private int theatreId;
-    private int movieId;
+    private Movie movie;
 
     private String screenNumber;
     private String showDate;
     private String showTime;
 
+
+    public Showtime(int showId, Movie movie, String screenNumber, String showDate, String showTime)
+    {
+        this.showId = showId;
+        this.movie= movie;
+        this.screenNumber = screenNumber;
+        this.showDate = showDate;
+        this.showTime = showTime;
+    }
+
     public int getShowId() {
         return showId;
     }
-
     public void setShowId(int showId) {
         this.showId = showId;
     }
 
-    public int getTheatreId() {
-        return theatreId;
+    public Movie getMovie() {
+        return movie;
     }
-
-    public void setTheatreId(int theatreId) {
-        this.theatreId = theatreId;
-    }
-
-    public int getMovieId() {
-        return movieId;
-    }
-
-    public void setMovieId(int movieId) {
-        this.movieId = movieId;
+    public void setMovie(Movie movie) {
+        this.movie = movie;
     }
 
     public String getScreenNumber() {
         return screenNumber;
     }
-
     public void setScreenNumber(String screenNumber) {
         this.screenNumber = screenNumber;
     }
@@ -47,7 +42,6 @@ public class Showtime {
     public String getShowDate() {
         return showDate;
     }
-
     public void setShowDate(String showDate) {
         this.showDate = showDate;
     }
@@ -55,7 +49,6 @@ public class Showtime {
     public String getShowTime() {
         return showTime;
     }
-
     public void setShowTime(String showTime) {
         this.showTime = showTime;
     }
