64f50d3bb689c583eebc036521d55aa538ce040e
tnn7846 <144117892+tnn7846@users.noreply.github.com>
2026-04-26 01:27:25 +0400

    Add Seat repository

---
+++ Reel_n_Roll/src/main/java/com/nichia/reel_n_roll/repository/SeatRepository.java
@@ -0,0 +1,29 @@
+package com.nichia.reel_n_roll.repository;
+
+
+// Tanya Navani - Student ID: 405008840
+
+import com.nichia.reel_n_roll.model.Seat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+    List<Seat> findBySeatNumberContainingIgnoreCase(String seatNumber);
+
+    List<Seat> findBySeatTypeContainingIgnoreCase(String seatType);
+
+    @Query("SELECT s FROM Seat s WHERE s.booked = :booked")
+    List<Seat> findSeatsByBookedStatus(boolean booked);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Seat s SET s.booked = :booked WHERE s.seatId = :seatId")
+    int updateBookedBySeatId(Long seatId, boolean booked);
+}
