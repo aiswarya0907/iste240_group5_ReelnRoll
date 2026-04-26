c57e486d76bbf3dbdcb25c43688a68d9933e60f2
Sonali3012 <sonab1230@gmail.com>
2026-04-26 18:43:51 +0400

    Added custom update method to TheatreRepository

--- Reel_n_Roll/src/main/java/com/nichia/reel_n_roll/repository/TheatreRepository.java
+++ Reel_n_Roll/src/main/java/com/nichia/reel_n_roll/repository/TheatreRepository.java
@@ -8,6 +8,9 @@ package com.nichia.reel_n_roll.repository;
 
 import com.nichia.reel_n_roll.model.Theatre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,4 +18,8 @@ public interface TheatreRepository extends JpaRepository<Theatre, Long> {
 
     List<Theatre> findByTheatreNameContaining(String theatreName);
 
+    @Transactional
+    @Modifying
+    @Query("UPDATE Theatre t SET t.theatreName = ?1 WHERE t.theatreId = ?2")
+    int updateTheatreNameById(String theatreName, Long id);
 }
